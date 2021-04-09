996b35ec0e551923d4a48d886a95494cc4d1d49c
natepace <n8wpace@gmail.com>
2021-04-08 19:08:18 -0600

    react update

---
+++ index.js
@@ -0,0 +1,26 @@
+const dotenv = require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const port = process.env.PORT || 9000
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/", (_, res) => {
+    res.json({ data: "API accounted for" })
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is alive on port ${port}`)
+})
+
+// console.log("its alive!")
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(process.env.USERNAME)
+// console.log(process.env.PORT)
+// console.log(process.env.FOOD)
+// console.log(process.env.REALLY)
+// console.log(process.env.GREET)
